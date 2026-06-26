5d6010a155fc1f41168483f534c2839067d2fc79
Tamir Ben Eden <tamirkatom@gmail.com>
2026-06-26 13:32:28 +0300

    add another origin

--- Shva.TransactionSimulator.Api/Program.cs
+++ Shva.TransactionSimulator.Api/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
             policy
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-                .WithOrigins("http://localhost:5173");
+                .WithOrigins("http://localhost:5173", "http://localhost:5174");
         });
 });
 
